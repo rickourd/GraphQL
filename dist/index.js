bd5be8c9ee30b91866d418e07e027f405bf9ac88
rcancin1 <rcancin1@externos.bancoestado.cl>
2023-11-06 12:33:53 -0300

    primer commit

---
+++ dist/index.js
@@ -0,0 +1,83 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { RESTDataSource } from 'apollo-datasource-rest';
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Pokemon {
+    name: String
+    url: String
+  }
+
+  type ListPokemon {
+    count: Int
+    next: Int
+    previous: Int
+    results: [Pokemon]
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book],
+    pokemon: ListPokemon,
+  }
+`;
+let books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+class PokemonAPI extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = 'https://pokeapi.co/api/v2/'; // URL de la API externa
+    }
+    async getPokemon() {
+        return this.get(`pokemon?limit=100000&offset=0`);
+    }
+}
+const pokemonAPI = new PokemonAPI;
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+        books: () => {
+            books.push({
+                title: 'Banco Estado',
+                author: 'Adquerencia',
+            });
+            return books;
+        },
+        pokemon: async () => await pokemonAPI.getPokemon(),
+    },
+};
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
